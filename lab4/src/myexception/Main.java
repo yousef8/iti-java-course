d6e7a4eaaeff8b818b7acbedda4afe23b3c577d3
Yousef Elsayed <yousef4511@outlook.com>
2024-01-04 20:45:09 +0200

    Add Main file for the exception assignment

---
+++ lab4/src/myexception/Main.java
@@ -0,0 +1,35 @@
+package myexception;
+
+public class Main {
+    public static void main(String[] args) {
+        Test test = new Test();
+
+        try {
+            int res = test.divide(5, 0);
+            System.out.println("Division result = "  + res);
+        }
+        catch(MyException e){
+            System.out.println("Division Exception : " + e.getMessage());
+        }
+
+        try {
+            double res = test.sqrt(-5);
+            System.out.println("Square Root result : " + res);
+        }
+        catch (Exception e ){
+            System.out.println( "Square Root Exception : " + e.getMessage());
+        }
+
+        try {
+            double res = test.difference(5, 10);
+            System.out.println("Difference result is : " + res);
+
+        }
+        catch (MyException e){
+            System.out.println("Difference Exception : " + e.getMessage());
+        }
+        finally {
+            System.out.println("--------------End Of Test--------------------");
+        }
+    }
+}
