78bee096c2dec21551008c3245b15cc25d6ae5ae
Yousef Elsayed <yousef4511@outlook.com>
2024-01-10 01:10:11 +0200

    lab 6: Get most populated city in each country

--- lab6/src/Main.java
+++ lab6/src/Main.java
@@ -2,17 +2,19 @@ import dao.InMemoryWorldDao;
 import domain.City;
 import domain.Country;
 
+import java.util.Comparator;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        InMemoryWorldDao worldDao = InMemoryWorldDao.getInstance();
-        final Map<String, Country> countries = worldDao.getCountries();
-        final Map<Integer, City> cities = worldDao.getCities();
+  public static void main(String[] args) {
+    InMemoryWorldDao worldDao = InMemoryWorldDao.getInstance();
+    final Map<String, Country> countries = worldDao.getCountries();
 
-        Stream<Country> countriesStream = countries.values().stream();
-        countriesStream.filter(country -> country.getPopulation() > 1_000_000_000).forEach(System.out::println);
-
-    }
-    }
+    // Highest Populations city in every Country
+    System.out.println("######################  Highest Population City in Each Country  ###############################");
+    countries.values().stream().map((country) -> Map.entry(country.getName(), country.getCities().stream().max(Comparator.comparingInt(City::getPopulation))))
+        .filter((entry) -> entry.getValue().isPresent())
+        .map((entry) -> Map.entry(entry.getKey(), entry.getValue().get()))
+        .forEach(System.out::println);
+  }
+}
